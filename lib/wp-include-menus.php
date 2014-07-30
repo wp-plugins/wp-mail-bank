147656bddecdacdf15c5aec99f667d719d55cb57
contact-banker <contact-banker@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-30 03:34:37 +0000

    Initial Version Release
    git-svn-id: https://plugins.svn.wordpress.org/wp-mail-bank/trunk@957092 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/wp-include-menus.php
@@ -0,0 +1,64 @@
+<?php
+//--------------------------------------------------------------------------------------------------------------//
+// CODE FOR CREATING MENUS
+//---------------------------------------------------------------------------------------------------------------//
+global $wpdb,$current_user;
+$role = $wpdb->prefix . "capabilities";
+$current_user->role = array_keys($current_user->$role);
+$role = $current_user->role[0];
+
+switch($role)
+{
+	case "administrator":
+		add_menu_page(" Mail Bank", __(" Mail Bank", mail_bank), "read", "smtp_mail","", plugins_url("/assets/images/mail.png" , dirname(__FILE__)));
+		add_submenu_page("smtp_mail", "Settings", __("Settings", mail_bank), "read", "smtp_mail","smtp_mail");
+		add_submenu_page("", "", "", "read", "send_test_email",  "send_test_email");
+		add_submenu_page("smtp_mail", "System Status", __("System Status", mail_bank), "read", "mail_system_status", "mail_system_status" );
+		break;
+	case "editor":
+		add_menu_page(" Mail Bank", __(" Mail Bank", mail_bank), "read", "smtp_mail","", plugins_url("/assets/images/mail.png" , dirname(__FILE__)));
+		add_submenu_page("smtp_mail", "Settings", __("Settings", mail_bank), "read", "smtp_mail","smtp_mail");
+		add_submenu_page("", "", "", "read", "send_test_email",  "send_test_email");
+		add_submenu_page("smtp_mail", "System Status", __("System Status", mail_bank), "read", "mail_system_status", "mail_system_status" );
+		break;
+	case "author":
+		add_menu_page(" Mail Bank", __(" Mail Bank", mail_bank), "read", "smtp_mail","", plugins_url("/assets/images/mail.png" , dirname(__FILE__)));
+		add_submenu_page("smtp_mail", "Settings", __("Settings", mail_bank), "read", "smtp_mail","smtp_mail");
+		add_submenu_page("", "", "", "read", "send_test_email",  "send_test_email");
+		add_submenu_page("smtp_mail", "System Status", __("System Status", mail_bank), "read", "mail_system_status", "mail_system_status" );
+		break;
+}
+
+//--------------------------------------------------------------------------------------------------------------//
+// CODE FOR CREATING PAGES
+//---------------------------------------------------------------------------------------------------------------//
+
+function smtp_mail()
+{
+	global $wpdb,$current_user,$user_role_permission;
+	$role = $wpdb->prefix . "capabilities";
+	$current_user->role = array_keys($current_user->$role);
+	$role = $current_user->role[0];
+	include_once MAIL_BK_PLUGIN_DIR ."/views/mail_header.php";
+	include_once MAIL_BK_PLUGIN_DIR ."/views/mail_settings.php";
+}
+
+function mail_system_status()
+{
+	global $wpdb,$current_user,$user_role_permission,$wp_version;
+	$role = $wpdb->prefix . "capabilities";
+	$current_user->role = array_keys($current_user->$role);
+	$role = $current_user->role[0];
+	include_once MAIL_BK_PLUGIN_DIR . "/views/wp_system_status.php";
+}
+function send_test_email()
+{
+	global $wpdb,$current_user,$user_role_permission,$wp_version;
+	$role = $wpdb->prefix . "capabilities";
+	$current_user->role = array_keys($current_user->$role);
+	$role = $current_user->role[0];
+	include_once MAIL_BK_PLUGIN_DIR ."/views/mail_header.php";
+	include_once MAIL_BK_PLUGIN_DIR . "/views/test_email.php";
+}
+
+?>
---
+++ readme.txt
@@ -0,0 +1,52 @@
+﻿=== WP Mail Bank ===
+Contributors: contact-banker
+Tags: mail, smtp, wp_mail, mailer, phpmailer, admin, plugins
+Requires at least: 2.7
+Tested up to: 3.9.1
+Stable tag: 1.0
+
+WP Mail Bank reconfigures the wp_mail() function and make it more enhanced.
+
+== Description ==
+
+This plugin reconfigures the wp_mail() function nd make it more enhanced to use SMTP instead of mail() with various options available.
+
+Features :
+
+* Specify the From name
+* Specify the Email Address for outgoing email.
+* Choose to send mail by SMTP or PHP's mail() function.
+* Specify an SMTP host
+* Specify an SMTP port
+* Choose SSL / TLS encryption
+* Choose to use SMTP authentication or not
+* Specify an SMTP username and password.
+
+== Installation ==
+
+### Minimum requirements.  
+*   Wordpress 2.7+  
+*   PHP 5.x  
+*   MySQL 5.x  
+
+### Performing a new installation  
+
+After downloading the ZIP file,  
+
+1. Log in to the administrator panel.  
+2. Go to Plugins Add > New > Upload.  
+3. Click “Choose file” (“Browse”) and select the downloaded zip file.  
+
+*For Mac Users*  
+*Go to your Downloads folder and locate the folder with the plugin. Right-click on the folder and select Compress. Now you have a newly created .zip file which can be installed as described here.*  
+
+1. Click “Install Now” button.  
+2. Click “Activate Plugin” button for activating the plugin.  
+
+If any problem occurs, please contact us at [support@tech-banker.com](mailto:support@tech-banker.com).
+
+== Changelog ==
+
+= 1.0 =
+
+* Initial Release
